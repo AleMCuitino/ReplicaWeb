66bf23ea852d60f38481b1d380459efa67512740
AleMCuitino <ale.morales11@gmail.com>
2022-11-14 12:10:42 +0100

    add react document and install bootstrap

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        
+      </header>
+      <h1 className="mt-5">
+        Réplica Web
+      </h1>
+    </div>
+  );
+}
+
+export default App;
