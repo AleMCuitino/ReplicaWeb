1b2816fff655174402f547b282e691ec9df0288e
AleMCuitino <ale.morales11@gmail.com>
2022-11-17 14:59:37 +0100

    add circles desktop version

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -4,6 +4,11 @@ import girl from '../assets/images/landing-page-girl.png'
 function MainHome() {
     return (
         <>
+            
+                <div className="circle-1"></div>
+                <div className="circle-2"></div>
+                <div className="circle-3"></div>
+            
             <div className="home d-flex justify-content-center">
                 <img className="girl" src={girl} alt="girl listening muisc"/>
                 <div className="text-home">
@@ -11,7 +16,6 @@ function MainHome() {
                     <p className="description">Stream over 20 thousand songs with one click</p>
                     <button type="button" className="btn btn-primary">Join Now</button>
                 </div>
-                
             </div>
         </>    
     );
