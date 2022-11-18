390719442ac4e1243178b63b6a014a12dfd39245
AleMCuitino <ale.morales11@gmail.com>
2022-11-18 12:02:26 +0100

    Link join main home

--- src/components/MainJoin.jsx
+++ src/components/MainJoin.jsx
@@ -11,19 +11,19 @@ function MainJoin() {
                 </div>
                 <div className="form img-fluid">
                     <form>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Name</label>
-                            <input type="email" class="form-control bg-transparent" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <div className="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Name</label>
+                            <input type="email" className="form-control bg-transparent" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                         </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control bg-transparent" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <div className="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="email" className="form-control bg-transparent" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                         </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control bg-transparent" id="exampleInputPassword1"/>
+                        <div className="mb-3">
+                            <label for="exampleInputPassword1" className="form-label">Password</label>
+                            <input type="password" className="form-control bg-transparent" id="exampleInputPassword1"/>
                         </div>
-                        <button type="submit" class="btn btn-join">Join now</button>
+                        <button type="submit" className="btn btn-join">Join</button>
                     </form>
                 </div>
             </div>
